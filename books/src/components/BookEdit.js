acc9b42d1197173dd1190fbb0ea665cfb42f34d9
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-02-24 00:45:13 +0800

    Initial Commit

---
+++ books/src/components/BookEdit.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+function BookEdit() {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(title);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="book-edit">
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="Button is primary">Save</button>
+    </form>
+  );
+}
+
+export default BookEdit;
---
+++ books/src/components/BookList.js
@@ -0,0 +1,11 @@
+import BookShow from "./BookShow";
+
+function BookList({ books, onDelete }) {
+  const renderBooks = books.map((book) => {
+    return <BookShow onDelete={onDelete} key={book.id} book={book} />;
+  });
+
+  return <div className="book-list">{renderBooks}</div>;
+}
+
+export default BookList;
