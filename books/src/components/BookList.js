950eab285ff96e76f70b44f611de4df64cd60e47
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-02-26 16:30:34 +0800

    UseContext-Communication Using the Context System

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,10 +1,16 @@
+// import { useContext } from "react";
+// import BooksContext from "../context/books";
 import BookShow from "./BookShow";
+import useBooksContext from "../hooks/use-books-context";
 
-function BookList({ books, onDelete, onEdit }) {
+function BookList() {
+  const { books } = useBooksContext();
+  // const { count, incrementCount } = useContext(BooksContext);
+  // {count}
+  // <button onClick={incrementCount}>Click</button>
+
   const renderBooks = books.map((book) => {
-    return (
-      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
-    );
+    return <BookShow key={book.id} book={book} />;
   });
 
   return <div className="book-list">{renderBooks}</div>;
