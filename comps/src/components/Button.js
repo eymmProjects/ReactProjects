1c2664941c332eaf19de5c9c0407c34d1e9e0c45
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-03-10 23:29:06 +0800

    Cars

--- comps/src/components/Button.js
+++ comps/src/components/Button.js
@@ -1,5 +1,4 @@
-import propTypes from "prop-types";
-import className from "classnames";
+import className from 'classnames';
 
 function Button({
   children,
@@ -10,30 +9,25 @@ function Button({
   danger,
   outline,
   rounded,
-  ...rest
+  onClick,
 }) {
-  // console.log(rest);
-  const classes = className(
-    rest.className,
-    "flex items-center px-3 py-1.5 m-2 border",
-    {
-      "border-blue-500 bg-blue-500 text-white": primary,
-      "border-blue-900 bg-blue-900 text-white": secondary,
-      "border-green-500 bg-green-500 text-white": success,
-      "border-yellow-500 bg-yellow-500 text-white": warning,
-      "border-red-500 bg-red-500 text-white": danger,
-      // "bg-white ": outline,kinsa
-      "rounded-full": rounded,
-      "text-blue-500": outline && primary,
-      "text-gray-500": outline && secondary,
-      "text-green-500": outline && success,
-      "text-yellow-500": outline && warning,
-      "text-red-500": outline && danger,
-    }
-  );
+  const classes = className('flex items-center px-3 py-1.5 border', {
+    'border-blue-500 bg-blue-500 text-white': primary,
+    'border-gray-900 bg-gray-900 text-white': secondary,
+    'border-green-500 bg-green-500 text-white': success,
+    'border-yellow-400 bg-yellow-400 text-white': warning,
+    'border-red-500 bg-red-500 text-white': danger,
+    'rounded-full': rounded,
+    'bg-white': outline,
+    'text-blue-500': outline && primary,
+    'text-gray-900': outline && secondary,
+    'text-green-500': outline && success,
+    'text-yellow-400': outline && warning,
+    'text-red-500': outline && danger,
+  });
 
   return (
-    <button {...rest} className={classes}>
+    <button onClick={onClick} className={classes}>
       {children}
     </button>
   );
@@ -44,14 +38,14 @@ Button.propTypes = {
     const count =
       Number(!!primary) +
       Number(!!secondary) +
-      Number(!!success) +
       Number(!!warning) +
+      Number(!!success) +
       Number(!!danger);
 
-    // console.log(props);
-
     if (count > 1) {
-      return new Error("Invalid Props");
+      return new Error(
+        'Only one of primary, secondary, success, warning, danger can be true'
+      );
     }
   },
 };
