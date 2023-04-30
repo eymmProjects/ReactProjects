d12cb0123a9a89172b04cff3caca399d23692dc5
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-04-30 19:07:01 +0800

    Finished Food Recipe Hub

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,46 +1,104 @@
 import { useState } from "react";
 import Axios from "axios";
 import "./App.css";
-
-// Application ID
-// aaccdc88
-
-// Application Keys
-// de203fc7e90572b513a3c2ae9ccabcae
-
+// import Recipetile from "./Recipetile/Recipetile";
+import Recipetile from "./recipetilev2";
 function App() {
-  const YOUR_APP_ID = `aaccdc88`;
-  const YOUR_APP_KEY = `de203fc7e90572b513a3c2ae9ccabcae`;
+  const YOUR_APP_ID = `7184cf21`;
+  const YOUR_APP_KEY = `a9337d6a73353c7c9921eb69850b411c`;
+  const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
-  const url = `curl "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=alcohol-free"
-  `;
+  const [healthLabel, setHealthLabel] = useState("vegetarian");
 
-  const getRecipeInfo = async (e) => {
-    e.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault(); //this will prevent page from reloading.
+    getRecipeInfo();
+  };
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=${healthLabel}`;
+
+  const getRecipeInfo = async () => {
     var result = await Axios.get(url);
     setRecipes(result.data.hits);
     console.log(result.data.hits);
   };
   return (
     <div className="app">
-      <h1 onClick={getRecipeInfo}>result</h1>
       <h1>
         <u>Food Recipe Hub</u>
       </h1>
-
-      <div className="app__searchForm">
+      <form className="app__searchForm" onSubmit={onSubmit}>
         <input
           type="text"
           placeholder="Type the Ingredient"
           autoComplete="Off"
-          className="app__input"
+          className="app__input myInput"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
         />
 
-        <select name="" id="" className="app__healthLabels">
-          <option value="vegan">Vegan</option>
+        <select className="app__healthLabels">
+          <option
+            value="vegan"
+            onClick={() => {
+              setHealthLabel("vegan");
+            }}
+          >
+            vegan
+          </option>
+          <option
+            value="vegetarian"
+            onClick={() => {
+              setHealthLabel("vegetarian");
+            }}
+          >
+            vegetarian
+          </option>
+          <option
+            value="low-sugar"
+            onClick={() => {
+              setHealthLabel("low-sugar");
+            }}
+          >
+            low-sugar
+          </option>
+          <option
+            value="dairy-free"
+            onClick={() => {
+              setHealthLabel("dairy-free");
+            }}
+          >
+            dairy-free
+          </option>
+          <option
+            value=" immuno-supportive "
+            onClick={() => {
+              setHealthLabel(" immuno-supportive ");
+            }}
+          >
+            immuno-supportive
+          </option>
+          <option
+            value="wheat-free"
+            onClick={() => {
+              setHealthLabel("wheat-free");
+            }}
+          >
+            wheat-free
+          </option>
         </select>
 
-        <input type="text" value="Get Recipe" className="app__submit" />
+        <input type="submit" value="Get Recipe" className="app__submit" />
+      </form>
+
+      <div className="app__recipes">
+        {recipes.map((recipe, index) => {
+          return (
+            <Recipetile key={recipe.recipe.uri} recipe={recipe} index={index} />
+          );
+        })}
       </div>
     </div>
   );
