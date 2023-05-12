5eca8f3bfdea84f01dc4ed275da378dc7c1a121e
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-05-12 18:36:15 +0800

    Show setShow

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 5271, name: "Record React Lectures", completed: true },
+    { id: 7825, name: "Edit React Lectures", completed: false },
+    { id: 8391, name: "Watch Lectures", completed: false },
+  ]);
+
+  const [show, setShow] = useState(true);
+
+  function handleDelete(id) {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task List</h1>
+      <ul>
+        <button onClick={() => setShow(!show)} className="trigger">
+          Toggle
+        </button>
+
+        {show &&
+          tasks.map((task) => (
+            <li
+              key={task.id}
+              className={task.completed ? "completed" : "incomplete"}
+            >
+              <span>
+                {task.id} - {task.name}
+              </span>
+              <button onClick={() => handleDelete(task.id)} className="delete">
+                Delete
+              </button>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
