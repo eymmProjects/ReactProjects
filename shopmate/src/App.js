a21e9f9085401b303fd679142efb4b21fb8304ec
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-05-18 22:31:25 +0800

    useEffect, callback, and custom hook

--- shopmate/src/App.js
+++ shopmate/src/App.js
@@ -1,12 +1,53 @@
-import "./App.css";
-import { ProductList } from "./components/ProductList";
+import { ProductList } from './components/ProductList';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Product List</h1>
+      <ProductList />
     </div>
   );
 }
 
 export default App;
+
+// import { useEffect, useState } from "react"
+
+// export const ProductList = () => {
+//     const [products, setProducts] = useState([]);
+
+//     useEffect(() => {
+//         fetch("http://localhost:8000/products")
+//         .then(response => response.json())
+//         .then(data => setProducts(data));
+//     }, []);
+
+//   return (
+//     <section>
+//         { products.map((product) => (
+//             <div className="card" key={product.id}>
+//                 <p className="id">{product.id}</p>
+//                 <p className="name">{product.name}</p>
+//                 <p className="info">
+//                     <span>${product.price}</span>
+//                     <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
+//                 </p>
+//             </div>
+//         )) }
+//     </section>
+//   )
+// }
+
+// { products.map((product) => {
+//   <div className="card" key={product.id}>
+//     <p className="id">{product.id}</p>
+//     <p className="name">{product.name}</p>;
+//     <p className="info">
+//       <span>${product.price}</span>
+//       <span className={product.in_stock ? "inStock" : "unavailable"}>
+//         {product.in_stock ? "In Stock" : "Unavailable"}
+//       </span>
+//     </p>
+//   </div>;
+// }) }
--- shopmate/src/components/ProductList.js
+++ shopmate/src/components/ProductList.js
@@ -1,11 +1,63 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import { useFetch } from "../hooks/useFetch";
+import loadingGif from "../assets/loading.gif";
 
 export const ProductList = () => {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
+  const [url, setUrl] = useState("http://localhost:8000/products");
+  const { data: products, loading } = useFetch(url);
+  // console.log(products);
 
-  fetch("  http://localhost:8000/products")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  // useEffect(() => {
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((data) => setProducts(data));
+  // }, [url]);
+  //37
 
-  return <div>ProductList</div>;
+  // const fetchProducts = useCallback(async () => {
+  //   const response = await fetch(url);
+  //   const data = await response.json();
+  //   // setProducts(data);
+  // }, []);
+
+  // useEffect(() => {
+  //   fetchProducts();
+  //   console.log("-");
+  // }, [fetchProducts]);
+
+  return (
+    <section>
+      <div className="filter">
+        <button onClick={() => setUrl("http://localhost:8000/products")}>
+          All
+        </button>
+        <button
+          onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}
+        >
+          In Stock Only
+        </button>
+      </div>
+      {loading && (
+        <p className="loading">
+          <img src={loadingGif} alt="" />
+        </p>
+      )}
+      {loading && <p className="loading">Error</p>}
+
+      {products &&
+        products.map((product) => (
+          <div className="card" key={product.id}>
+            <p className="id">{product.id}</p>
+            <p className="name">{product.name}</p>
+            <p className="info">
+              <span>${product.price}</span>
+              <span className={product.in_stock ? "instock" : "unavailable"}>
+                {product.in_stock ? "In Stock" : "Unavailable"}
+              </span>
+            </p>
+          </div>
+        ))}
+    </section>
+  );
 };
